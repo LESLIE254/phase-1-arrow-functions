fed242343cc126aad567a3a31e138ed716d3255b
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-08-17 16:45:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const divide = function(num1 = 2000, num2 = 100) {
+  return num1 / num2;
+
+}
+const square = parameter1 => parameter1 *parameter1;
+const add = (parameter1, parameter2) => parameter1 + parameter2;
